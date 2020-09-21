f4cef2ea2064c6c835e421ae0cb9e7fee2241806
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-09-21 10:51:03 +0300

    ex 1.5 done

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -35,8 +35,9 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
     {
       name: 'Fundamentals of React',
       exercises: 10
@@ -50,13 +51,14 @@ const App = () => {
       exercises: 14
     }
   ]
+}
   
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts}/>
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts} />
     </div>
   )
 }
