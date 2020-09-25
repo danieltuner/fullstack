1ef632dd6704ce9b0171d41f7b107778999bb0cb
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-09-25 11:19:59 +0300

    ex 2.1 done

--- part2/courseinformation/src/index.js
+++ part2/courseinformation/src/index.js
@@ -33,34 +33,40 @@ const Total = (props) => {
     </div>
   )
 }
+const Course = ({ course }) => { 
+  console.log(course)
+  return (
+    <div>
+    <Header course={course.name} />
+    <Content parts={course.parts}/>
+    </div>
+  )
+}
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-}
-  
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
 
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts}/>
-      <Total parts={course.parts} />
-    </div>
-  )
+  return <Course course={course} />
 }
 
 
