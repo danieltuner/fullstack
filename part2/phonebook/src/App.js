8feb30e72aff87fcb850f3aea05354ab19a3b774
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-09-28 12:26:15 +0300

    phonebook 2.6 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,19 +6,41 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const personObject = {
+      name: newName,
+      date: new Date().toISOString(),
+      id: persons.length + 1,
+    }
+  setPersons(persons.concat(personObject))
+  setNewName('')
+  
+  }
+  const handlePersonChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName}
+          onChange={handlePersonChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      ...
+      <h2>Numbers</h2>     
+        {persons.map(person =>
+          <div key={person.name}> {person.name}
+          </div>
+          )}
+
     </div>
   )
 }
