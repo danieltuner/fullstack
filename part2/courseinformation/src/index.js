fb44c57e8dfd746e0c726a49577d28a976492840
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-09-28 13:59:48 +0300

    ex 2.5 done

--- part2/courseinformation/src/index.js
+++ part2/courseinformation/src/index.js
@@ -1,48 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course.name}</h1>
-    </div>
-  )
-}
-const Content = ({course}) => {
-  return (
-    <div>
-      {course.parts.map(course => 
-        <Part key={course.id} name={course.name} exercises={course.exercises} />)}   
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <div>
-      <p> {props.name} {props.exercises} </p>
-    </div>
-  )
-}
-
-const Total = ({course}) => {
-  const total = course.parts.reduce((sum, part) => {
-    console.log('this happens', sum, part)
-    return sum + part.exercises
-  }, 0)
-  return (<p><b> total of {total} exercises</b></p>)
-   
-}
-const Course = ({ course }) => { 
-  console.log(course)
-  return (
-    <div>
-    <Header course={course} />
-    <Content course={course}/>
-    <Total course={course} />
-    </div>
-  )
-}
+import Course from './Course'
 
 const App = () => {
   const courses = [
