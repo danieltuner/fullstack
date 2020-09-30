e419244a28e527cb2d470fd3defe62e61d03cc25
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-09-30 16:15:24 +0300

    countries start

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
-function App() {
+import axios from 'axios'
+
+const App = () => {
+  const [ countries, setCountries ] = useState([]) 
+
+
+
+  /*const hook = () => {
+    console.log('effect')
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        console.log('promise fulfilled')
+        set'Persons'(response.data)
+      })
+  }
+  
+  useEffect(hook, []) */
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
-  );
+  )
 }
+
+
 
 export default App;
