734a1a0770120f991cdb6610e8ea31383e39929e
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-10-30 14:57:11 +0200

    tadaa?

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -44,7 +44,7 @@ const App = () => {
 
     // !persons.map(person => person.name.toLowerCase().includes(newName.toLowerCase())
   // persons.every((p) => p.name.toLowerCase() !== newName.toLowerCase())
-  if (!persons.map(person => person.name.toLowerCase().includes(newName.toLowerCase())))
+  if (persons.map(person => person.name.toLowerCase().includes(newName.toLowerCase())))
     {
       personService
       .create(personObject)
