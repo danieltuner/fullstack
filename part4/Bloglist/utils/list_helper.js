107d2d30a65a7d032da1f40ad9120632aadf0e90
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-10-30 12:47:19 +0200

    4.4 done

--- part4/Bloglist/utils/list_helper.js
+++ part4/Bloglist/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
     return 1
   }
   
+  const totalLikes = (bloglist) => {
+    const reducer = (sum, blog) => {
+        return sum + blog.likes
+    }
+    return bloglist.reduce(reducer, 0)
+}
   module.exports = {
-    dummy
+    dummy, totalLikes
   }
