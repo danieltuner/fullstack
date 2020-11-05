448a4fc6c24686b6da9875f188c3f3b46b0321bc
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-11-05 11:15:00 +0200

    4.15 done

--- part4/Bloglist/App.js
+++ part4/Bloglist/App.js
@@ -6,22 +6,27 @@ const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
+const usersRouter = require('./controllers/users')
 require('express-async-errors')
 
 
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(result => {
-    console.log('connected to MongoDB')
+  .then(() => {
+    logger.info('connected to MongoDB')
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
+    logger.info('error connecting to MongoDB:', error.message)
   })
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
