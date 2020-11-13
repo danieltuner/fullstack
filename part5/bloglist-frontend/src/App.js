8d8e467c4745c1c3757e2d9fc8da79e0b52d78f3
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-11-13 09:28:29 +0200

    5.5 - 5.6 done

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,6 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Blog from './components/Blog'
 import Notification from './components/Notification'
+import BlogForm from './components/BlogForm'
+import LoginForm from './components/LoginForm'
+import Togglable from './components/Togglable'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import './App.css'
@@ -10,11 +13,10 @@ const App = () => {
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null)
-  const [newBlogTitle, setNewBlogTitle] = useState('')
-  const [newBlogAuthor, setNewBlogAuthor] = useState('')
-  const [newBlogUrl, setNewBlogUrl] = useState('')
   const [message, setMessage] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
+
+  const blogFormRef = useRef()
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -53,22 +55,19 @@ const App = () => {
     }
   }
 
-  const addNewBlog = (event) => {
-    event.preventDefault()
-    const blogObject = {
-      title: newBlogTitle,
-      author: newBlogAuthor,
-      url: newBlogUrl
-    }
+  const blogForm = () => (
+    <Togglable buttonLabel='New Blog' ref={blogFormRef}>
+      <BlogForm createBlog={addBlog} />
+    </Togglable>
+  )
 
+  const addBlog = (blogObject) => {
+    blogFormRef.current.toggleVisibility()
     blogService
       .create(blogObject)
       .then(savedBlog => {
         setBlogs(blogs.concat(savedBlog))
-        setMessage(`The new blog: ${newBlogTitle}, was added by ${newBlogAuthor}.`)
-        setNewBlogTitle('')
-        setNewBlogAuthor('')
-        setNewBlogUrl('')
+        setMessage(`The new blog: ${blogObject.title}, was added by ${blogObject.author}.`)
         setTimeout(() => {
           setMessage(null)
         }, 4000)
@@ -78,29 +77,16 @@ const App = () => {
   if (user === null) {
     return (
       <div>
-        <h2>Log in: </h2>
         <Notification message={message} errorMessage={errorMessage} />
-        <form onSubmit={handleLogin}>
-        <div>
-          username
-            <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
+        <Togglable buttonLabel='login'>
+        <LoginForm
+            username={username}
+            password={password}
+            handleUsernameChange={({ target }) => setUsername(target.value)}
+            handlePasswordChange={({ target }) => setPassword(target.value)}
+            handleSubmit={handleLogin}
           />
-        </div>
-        <div>
-          password
-            <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">login</button>
-        </form>
+          </Togglable>
       </div>
     )
   }
@@ -114,37 +100,8 @@ const App = () => {
         window.localStorage.removeItem('loggedBlogappUser')
         setUser(null)
       }}>logout</button></p>
-      <h2>create new</h2>
-      <form onSubmit={addNewBlog}>
-      <div>
-      title:
-          <input
-              name="title"
-              onChange={({ target }) => setNewBlogTitle(target.value)}
-              type="text"
-              value={newBlogTitle}
-          />
-      </div>
-      <div>
-          author:
-          <input
-              name="author"
-              onChange={({ target }) => setNewBlogAuthor(target.value)}
-              type="text"
-              value={newBlogAuthor}
-          />
-        </div>
-        <div>
-          url:
-          <input
-              name="url"
-              onChange={({ target }) => setNewBlogUrl(target.value)}
-              type="text"
-              value={newBlogUrl}
-          />
-        </div>
-        <button type="submit">create</button>
-      </form>
+      
+      {blogForm()}
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
