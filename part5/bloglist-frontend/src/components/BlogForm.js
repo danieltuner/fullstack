d3f3e79741b249ac53d0154ef7191d76ce6f6b37
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-11-16 10:29:45 +0200

    5.15 done

--- part5/bloglist-frontend/src/components/BlogForm.js
+++ part5/bloglist-frontend/src/components/BlogForm.js
@@ -27,6 +27,7 @@ const BlogForm = ({ createBlog }) => {
         <div>
         title:
           <input
+            id='title'
             name="title"
             onChange={({ target }) => setTitle(target.value)}
             type="text"
@@ -36,6 +37,7 @@ const BlogForm = ({ createBlog }) => {
         <div>
             author:
           <input
+            id='author'
             name="author"
             onChange={({ target }) => setAuthor(target.value)}
             type="text"
@@ -45,6 +47,7 @@ const BlogForm = ({ createBlog }) => {
         <div>
             url:
           <input
+            id='url'
             name="url"
             onChange={({ target }) => setUrl(target.value)}
             type="text"
